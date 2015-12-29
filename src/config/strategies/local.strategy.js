981cada830f481f203b5f16254691f6e27417673
Ashwin Rangarajan <r2ashwin@hotmail.com>
2015-12-28 22:25:11 -0600

    adds strategy for signup

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,12 +1,20 @@
 var passport = require('passport');
 var localStrategy = require('passport-local').Strategy;
-
 var localStrategyController = require('../../controllers/localstrategy.controller.js')();
 
 module.exports = function () {
     console.log('Authenticating!');
-    passport.use(new localStrategy({
+    console.log(localStrategyController);
+
+    passport.use('local.signin', new localStrategy({
         usernameField: 'username',
-        passwordField: 'password'
-    }, localStrategyController.authenticateLogin));
+        passwordField: 'password',
+        passReqToCallback: true
+    }, localStrategyController.authenticateSignin));
+
+    passport.use('local.signup', new localStrategy({
+        usernameField: 'username',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, localStrategyController.authenticateSingup));
 };
