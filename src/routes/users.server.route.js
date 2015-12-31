d8fab887c96618396b2ee822e9771bdd53384fd1
Ashwin Rangarajan <r2ashwin@hotmail.com>
2015-12-31 00:58:08 -0600

    adds feature to view list of blog posts by user

--- src/routes/users.server.route.js
+++ src/routes/users.server.route.js
@@ -2,6 +2,7 @@ var express = require('express');
 var userRouter = express.Router();
 
 var User = require('../models/user.server.model.js');
+var Blog = require('../models/blog.server.model.js');
 
 module.exports = () => {
     userRouter.route('/')
@@ -14,6 +15,24 @@ module.exports = () => {
                     users: users
                 })
             });
+        });
+
+    userRouter.route('/:id')
+        .get(function (req, res) {
+            var query = {
+                authorId: req.params.id
+            };
+
+            Blog.find(query, function (err, blogs) {
+                if (!err) {
+                    res.render('blogs', {
+                        title: 'Bloggedin',
+                        caption: 'Powered by Node.js',
+                        copyright: 'ASHw.xyz',
+                        blogs: blogs
+                    });
+                }
+            })
         });
 
     return userRouter;
