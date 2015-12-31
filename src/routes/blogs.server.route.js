c8151e1a1d57a3c7b6f6b084617c9dd88e64e3df
Ashwin Rangarajan <r2ashwin@hotmail.com>
2015-12-31 01:42:05 -0600

    adds /blogs/

--- src/routes/blogs.server.route.js
+++ src/routes/blogs.server.route.js
@@ -4,11 +4,25 @@ var blogsRouter = express.Router();
 var Blog = require('../models/blog.server.model.js');
 
 module.exports = (nav, info) => {
+    blogsRouter.route('/')
+        .get(function (req, res) {
+            Blog.find({}, function (err, blogs) {
+                if (!err) {
+                    res.render('blogs', {
+                        nav: nav,
+                        title: info.title,
+                        caption: info.caption,
+                        copyright: info.copyright,
+                        blogs: blogs
+                    });
+                }
+            });
+        });
+
     blogsRouter.route('/:id')
         .get(function (req, res, next) {
             Blog.findById(req.params.id, function (err, blog) {
                 if (!err) {
-                    console.log(blog);
                     res.render('my/post', {
                         nav: nav,
                         title: info.title,
