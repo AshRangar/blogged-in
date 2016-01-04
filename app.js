cc8cf16a959f8139e98bbba962429d321ec0fc05
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-03 18:45:50 -0600

    removes old code

--- app.js
+++ app.js
@@ -74,15 +74,6 @@ app.use('/users', usersRouter);
 var blogsRouter = require('./src/routes/blogs.server.route.js')(nav, info);
 app.use('/blogs', blogsRouter);
 
-//app.get('/', (req, res) => {
-//    res.render('login', {
-//        nav: nav,
-//        title: info.title,
-//        caption: info.caption,
-//        copyright: info.copyright,
-//    });
-//});
-
 app.listen(port, () => {
     console.log('Listening on port ' + port);
 });
