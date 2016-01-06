8d8447b2b51d8f2385f406b7f8bafc2bae459441
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-05 23:56:27 -0600

    adds authorlink and copyrightlink

--- app.js
+++ app.js
@@ -54,10 +54,13 @@ var nav = [
 ];
 
 var info = {
+    author: 'Ashwin Rangarajan',
+    authorlink: 'http://AshRangar.com',
     navtitle: 'Bloggedin',
     heading: 'Bloggedin',
     subheading: 'A Node.js Creation',
     copyright: 'Ashwin Rangarajan',
+    copyrightlink: 'http://AshRangar.com',
     background: '/img/red-cube.jpg'
 };
 
