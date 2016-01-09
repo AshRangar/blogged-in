dd332319767aad7ad94b7af882b038245ef16970
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-09 00:23:15 -0600

    redirects users without username to /auth/username to create new username

--- src/routes/auth.server.route.js
+++ src/routes/auth.server.route.js
@@ -2,6 +2,11 @@ var express = require('express');
 var authRouter = express.Router();
 var passport = require('passport');
 
+var info = require('../config/info.config.js')();
+var nav = require('../config/nav.config.js')();
+
+var User = require('../models/user.server.model.js');
+
 module.exports = () => {
 
     authRouter.route('/signIn')
@@ -24,6 +29,52 @@ module.exports = () => {
             res.redirect('/');
         });
 
+    authRouter.route('/username')
+        // If the user is not logged in, redirect to login page
+        // If the user is logged in, go next
+        .all(function (req, res, next) {
+            if (!req.user) {
+                res.redirect('/');
+            } else {
+                console.log(req.user);
+                next();
+            }
+        })
+        // If the user doesn't have a username, redirect to pages/username
+        // If the user has username, redirect to /my/posts
+        .get(function (req, res) {
+            if (!req.user.username) {
+                res.render('pages/username', {
+                    info: info,
+                    nav: nav
+                });
+            } else {
+                res.redirect('/my/posts');
+            }
+        })
+        // If the user has username, redirect to /my/posts
+        // If the user doesn't have one, add the entered username to user document 
+        .post(function (req, res) {
+            if (!req.user.username) {
+                var query = {
+                    _id: req.user._id
+                }
+                User.findOne(query, function (err, user) {
+                    if (!err) {
+                        user.username = req.body.username;
+                        user.save();
+                        res.redirect('/my/posts');
+                    } else {
+                        res.redirect('/auth/username');
+                    }
+                });
+            } else {
+                res.redirect('/my/posts');
+            }
+
+
+        });
+
     authRouter.route('/profile')
         .all((req, res, next) => {
             if (!req.user) {
@@ -38,7 +89,7 @@ module.exports = () => {
 
     authRouter.route('/google/callback')
         .get(passport.authenticate('google', {
-            successRedirect: '/my/posts',
+            successRedirect: '/auth/username',
             failureRedirect: '/error'
         }));
 
@@ -49,7 +100,7 @@ module.exports = () => {
 
     authRouter.route('/twitter/callback')
         .get(passport.authenticate('twitter', {
-            successRedirect: '/my/posts',
+            successRedirect: '/auth/username',
             failureRedirect: '/error'
         }));
 
@@ -58,7 +109,7 @@ module.exports = () => {
 
     authRouter.route('/facebook/callback')
         .get(passport.authenticate('facebook', {
-            successRedirect: '/my/posts',
+            successRedirect: '/auth/username',
             failureRedirect: '/error'
         }));
 
