e69c4a49235ad073cc4ea143977cedc3097c19bb
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-10 17:13:21 -0600

    removes debug comments

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -3,9 +3,6 @@ var localStrategy = require('passport-local').Strategy;
 var localStrategyController = require('../../controllers/localstrategy.server.controller.js')();
 
 module.exports = function () {
-    console.log('Authenticating!');
-    console.log(localStrategyController);
-
     passport.use('local.signin', new localStrategy({
         usernameField: 'username',
         passwordField: 'password',
