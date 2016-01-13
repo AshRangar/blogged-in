70834e3f25307fa8810fbec8054211f295bf3f2a
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-13 10:23:44 -0600

    Changes the top right link to the github

--- src/config/info.config.js
+++ src/config/info.config.js
@@ -1,10 +1,10 @@
 module.exports = function () {
     var info = {
-        author: 'Ashwin Rangarajan',
-        authorlink: 'http://AshRangar.com',
+        author: 'Bloggedin Github',
+        authorlink: 'https://github.com/AshRangar/blogged-in',
         navtitle: 'Bloggedin',
         heading: 'Bloggedin',
-        subheading: 'A Node.js Creation',
+        subheading: 'Built using Express.Js',
         copyright: 'Ashwin Rangarajan',
         copyrightlink: 'http://AshRangar.com',
         background: '/img/red-cube.jpg'
