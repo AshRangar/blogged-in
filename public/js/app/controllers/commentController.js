6fad8745d62da2d445862e9d0697d94e24fa75c5
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-21 00:03:05 -0600

    removes dummy comments

--- public/js/app/controllers/commentController.js
+++ public/js/app/controllers/commentController.js
@@ -1,22 +1,2 @@
 angular.module('app').controller('CommentController', ['$scope', '$http', function ($scope, $http) {
-    $scope.comments = [
-        {
-            username: 'Ash',
-            date: Date.now(),
-            comment: 'Cool beans',
-            link: '/google.com'
-                    },
-        {
-            username: 'Ash',
-            date: Date.now(),
-            comment: 'Cool beans',
-            link: 'html'
-                    },
-        {
-            username: 'Ash',
-            date: Date.now(),
-            comment: 'Cool beans',
-            link: 'html'
-                    }
-        ];
 }]);
