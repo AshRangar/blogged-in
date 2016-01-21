ca09fc5663a645e6051c71349fbfe5a88b222dd6
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-20 21:16:59 -0600

    adds postId to schema

--- src/models/comment.server.model.js
+++ src/models/comment.server.model.js
@@ -3,10 +3,13 @@ var Schema = mongoose.Schema;
 
 var blogSchema = mongoose.Schema({
     authorId: {
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId
     },
     authorUsername: {
         type: String
+    },
+    postId: {
+        type: Schema.Types.ObjectId
     },
     comment: {
         type: String
