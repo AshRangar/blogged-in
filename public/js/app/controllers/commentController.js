0b519a6aed4d71746e6d35e6cc503b613442f16a
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-21 00:04:08 -0600

    adds method to get the comments through rest call

--- public/js/app/controllers/commentController.js
+++ public/js/app/controllers/commentController.js
@@ -4,7 +4,17 @@ angular.module('app').controller('CommentController', ['$scope', '$http', functi
 
     console.log('Post ID is ' + $scope.postId);
     console.log($scope.postId);
+
     $scope.$watch('$scope.postId ', function () {
         getComments();
     });
+
+    var getComments = function () {
+        $http.get('/comments/' + $scope.postId)
+            .then(function (response) {
+                $scope.comments = response.data;
+                console.log("Comments are ");
+                console.log($scope.comments);
+            });
+    };
 }]);
