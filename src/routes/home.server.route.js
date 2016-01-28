a5294101da44ef9c5880c14164c920dd6aac7285
Ashwin Rangarajan <r2ashwin@hotmail.com>
2016-01-27 23:51:47 -0600

    change function style

--- src/routes/home.server.route.js
+++ src/routes/home.server.route.js
@@ -4,7 +4,7 @@ var homeRouter = express.Router();
 var info = require('../config/info.config.js')();
 var nav = require('../config/nav.config.js')();
 
-module.exports = () => {
+module.exports = function () {
     homeRouter.route('/')
         .all((req, res, next) => {
             if (req.user) {
